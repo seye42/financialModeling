342426f2ac7ca12b7be65c419e131c7b0c0882fb
seye42 <seaneye@gmail.com>
2017-03-14 02:09:37 -0400

    Refactored plotting
    Split plotting work into separate file, generalized for reuse, and added
    automatic range scaling support

--- finUtils.py
+++ finUtils.py
@@ -1,13 +1,13 @@
 import math
 import numpy as np
 import matplotlib.pyplot as plt
-
+import plotUtils
 
 
 def convAPRtoMon(rAnnual):
-    """
+    '''
     Convert APR to equivalent monthly rate
-    """
+    '''
 
     # solve (1 + rAnnual) = (1 + rMonthly) ** 12
     rMonthly = (1.0 + rAnnual) ** (1.0 / 12.0) - 1.0
@@ -15,16 +15,131 @@ def convAPRtoMon(rAnnual):
     return rMonthly
 
 
+def timeSeries(params, accounts):
+    # build age array
+    ages = np.arange(params['startAge'], params['stopAge'], 1.0 / 12)
+        # in years, but always with monthly steps
+
+    # initialize aggregate time series arrays
+    incomes  = np.zeros_like(ages)
+    expenses = np.zeros_like(ages)
+    discret  = np.zeros_like(ages)
+    netWorth = np.zeros_like(ages)
+
+    # initialize interest-bearing accounts
+    for a in accounts:
+        if 'intAPR' in a:
+            # convert APR to monthly rate
+            a['intMonthly'] = convAPRtoMon(a['intAPR'])
+
+            # initialize balance time series
+            a['balances'] = np.zeros_like(ages)
+            a['balances'][0] = a['initBalance']
+            netWorth[0] += a['initBalance']
+        if a['type'] == 'loan':
+            a['payments'] = np.zeros_like(ages)
+
+    # run time series calculations
+    for idx in range(1, len(ages)):  # skip the first age since it's just initial value data
+        # get total income and expenses for this period
+        for a in accounts:
+            if a['type'] == 'income':
+                incomes[idx] += a['delMonthly']
+            elif a['type'] == 'expense':
+                expenses[idx] += a['delMonthly']
+
+        # accrue interest on accounts
+        for a in accounts:
+            if 'intMonthly' in a:
+                a['balances'][idx] = a['balances'][idx - 1] * (1.0 + a['intMonthly'])
+
+        # determine net cash flow this month
+        discret[idx] = incomes[idx] + expenses[idx]
+        avail = discret[idx]
+
+        # pay down any outstanding loans
+        if avail > 0.0:
+            for a in accounts:
+                if a['type'] == 'loan' and a['balances'][idx] < 0.0:
+                    # loan with outstanding balance
+                    payment = max([-avail, a['balances'][idx]])
+                        # max() because balances are negative
+                    a['balances'][idx] -= payment
+                    a['payments'][idx] = payment
+                    avail += payment
+
+                    # notify if loan was just paid off
+                    if a['balances'][idx] >= 0.0:
+                        print('%s loan paid off at age %0.2f\n' % (a['label'], ages[idx]))
+
+        # maximize Roth IRA contributions
+        if avail > 0.0:
+            for a in accounts:
+                if a['type'] == 'Roth':
+                    contrib = min([incomes[idx], avail, a['maxContrib']])
+                        # can't contribute unless there's earned income to cover it
+                    a['balances'][idx] += contrib
+                    avail -= contrib
+
+        # add whatever remains to savings
+        if avail > 0.0:
+            for a in accounts:
+                if a['type'] == 'savings':
+                    a['balances'][idx] += avail
+                    avail = 0.0
+                    break
+
+        # CORE ASSUMPTION HERE is that the discretionary money use prioritization is
+        # monthly expenses, then loans, then Roths, then taxable savings
+        # TODO: revisit this
+
+        # calculate net worth
+        for a in accounts:
+            if 'balances' in a:
+                netWorth[idx] += a['balances'][idx]
+
+    # plot monthly rates
+    plotUtils.multiPlot(ages, [incomes, expenses, discret], 'age (yr)', 'rate (2016 $/mo)',
+                        ['total income', 'total expenses', 'discretionary'])
+
+    # plot loan class payments and account balances
+    loans = []
+    loanNames = []
+    payments = []
+    paymentNames = []
+    for a in accounts:
+        if a['type'] == 'loan':
+            loans.append(a['balances'])
+            loanNames.append(a['label'])
+            payments.append(a['payments'])
+            paymentNames.append(a['label'])
+    plotUtils.multiPlot(ages, loans, 'age (yr)', 'balance (2016 $)', loanNames)
+    plotUtils.multiPlot(ages, payments, 'age (yr)', 'payments (2016 $/mo)', paymentNames)
+
+    # plot savings class account balances
+    savings = []
+    savingNames = []
+    for a in accounts:
+        if a['type'] in ['savings', 'Roth', 'IRA']:
+            savings.append(a['balances'])
+            savingNames.append(a['label'])
+    plotUtils.multiPlot(ages, savings, 'age (yr)', 'balance (2016 $)', savingNames)
+
+    # plot net worth
+    plotUtils.singlePlot(ages, netWorth, 'age (yr)', 'net worth (2016 $)')
+
+    return accounts
+
 
 def getReqMinDistrib(age, P):
-    """
-    Get the required minimum distribution for a 401(k) according to IRS rules.
-    Returned RMDs are normalized to monthly amounts.
+    '''
+    Get the required minimum distribution for a 401(k) according to IRS rules. Returned RMDs are
+    normalized to monthly amounts.
     - age is the account holder's current age in years
     - P is the account balance/principal as of 31 Dec the previous year
 
     Calculations are based on IRS publication 590-B, Table III from 2015
-    """
+    '''
 
     # Technically, rules allow for first RMD to occur by 1 Apr of the year
     # following the one in which the person turns 70.5.  This can sometimes
@@ -94,134 +209,3 @@ def getReqMinDistrib(age, P):
         RMD = P / d[idx] / 12.0
 
     return RMD
-
-
-
-def timeSeries(params, accounts):
-    # build age array
-    ages = np.arange(params["startAge"], params["stopAge"], 1.0 / 12)
-        # in years, but always with monthly steps
-
-    # initialize aggregate time series arrays
-    incomes  = np.zeros_like(ages)
-    expenses = np.zeros_like(ages)
-    discret  = np.zeros_like(ages)
-    netWorth = np.zeros_like(ages)
-
-    # initialize interest-bearing accounts
-    for a in accounts:
-        if "intAPR" in a:
-            # convert APR to monthly rate
-            a["intMonthly"] = convAPRtoMon(a["intAPR"])
-
-            # initialize balance time series
-            a["balances"] = np.zeros_like(ages)
-            a["balances"][0] = a["initBalance"]
-            netWorth[0] += a["initBalance"]
-        if a["type"] == "loan":
-            a["payments"] = np.zeros_like(ages)
-
-    # run time series calculations
-    for idx in range(1, len(ages)):  # skip the first age since it's just initial value data
-        # get total income and expenses for this period
-        for a in accounts:
-            if a["type"] == "income":
-                incomes[idx] += a["delMonthly"]
-            elif a["type"] == "expense":
-                expenses[idx] += a["delMonthly"]
-
-        # accrue interest on accounts
-        for a in accounts:
-            if "intMonthly" in a:
-                a["balances"][idx] = a["balances"][idx - 1] * (1.0 + a["intMonthly"])
-
-        # determine net cash flow this month
-        discret[idx] = incomes[idx] + expenses[idx]
-        avail = discret[idx]
-
-        # pay down any outstanding loans
-        if avail > 0.0:
-            for a in accounts:
-                if a["type"] == "loan" and a["balances"][idx] < 0.0:
-                    # loan with outstanding balance
-                    payment = max([-avail, a["balances"][idx]])  # max() because balances are negative
-                    a["balances"][idx] -= payment
-                    a["payments"][idx] = payment
-                    avail += payment
-
-                    # notify if loan was just paid off
-                    if a["balances"][idx] >= 0.0:
-                        print("%s loan paid off at age %0.2f\n" % (a["label"], ages[idx]))
-
-        # maximize Roth IRA contributions
-        if avail > 0.0:
-            for a in accounts:
-                if a["type"] == "Roth":
-                    contrib = min([incomes[idx], avail, a["maxContrib"]])
-                        # can't contribute unless there's earned income to cover it
-                    a["balances"][idx] += contrib
-                    avail -= contrib
-
-        # add whatever remains to savings
-        if avail > 0.0:
-            for a in accounts:
-                if a["type"] == "savings":
-                    a["balances"][idx] += avail
-                    avail = 0.0
-
-        # CORE ASSUMPTION HERE is that the discretionary money use prioritization is
-        # monthly expenses, then loans, then Roths, then taxable savings
-        # TODO: revisit this
-
-        # calculate net worth
-        for a in accounts:
-            if "balances" in a:
-                netWorth[idx] += a["balances"][idx]
-
-    # plot monthly rates
-    plt.figure()
-    plt.plot(ages, incomes, 'r', ages, expenses, 'g', ages, discret, 'b')
-    plt.xlabel("age (yr)")
-    plt.ylabel("rate (2016 $/mo)")
-    plt.legend(["total income", "total expenses", "discretionary"], loc="best")
-
-    # plot loan class payments and account balances
-    plt.figure()
-    plt.subplot(2, 1, 1)
-    plt.hold(True)
-    labels = []
-    for a in accounts:
-        if a["type"] == "loan":
-            plt.plot(ages, a["balances"])
-            labels.append(a["label"])
-    plt.xlabel("age (yr)")
-    plt.ylabel("balance (2016 $)")
-    plt.legend(labels, loc="best")
-    plt.subplot(2, 1, 2)
-    plt.hold(True)
-    for a in accounts:
-        if a["type"] == "loan":
-            plt.plot(ages, a["payments"])
-    plt.xlabel("age (yr)")
-    plt.ylabel("payment (2016 $/mo)")
-    plt.legend(labels, loc="best")
-
-    # plot savings class account balances
-    plt.figure()
-    plt.hold(True)
-    labels = []
-    for a in accounts:
-        if a["type"] in ["savings", "Roth", "IRA"]:
-            plt.plot(ages, a["balances"])
-            labels.append(a["label"])
-    plt.xlabel("age (yr)")
-    plt.ylabel("balance (2016 $)")
-    plt.legend(labels, loc="best")
-
-    # plot net worth
-    plt.figure()
-    plt.plot(ages, netWorth / 1.0e6, 'r')
-    plt.xlabel("age (yr)")
-    plt.ylabel("net worth (2016 $M)")
-
-    return accounts
---
+++ plotUtils.py
@@ -0,0 +1,48 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def getAutoRange(maxVal, label):
+    if maxVal > 1.0e6:
+        norm = 1.0e6
+        label = label.replace('$', '$M')
+    elif maxVal > 1.0e3:
+        norm = 1.0e3
+        label = label.replace('$', '$k')
+    else:
+        norm = 1.0
+
+    return (norm, label)
+
+
+def singlePlot(x, y, xLabel, yLabel):
+    # initialize the plot
+    plt.figure()
+
+    # determine automatic range scaling
+    (norm, yLabel) = getAutoRange(np.amax(np.fabs(y)), yLabel)
+
+    # plot the series
+    plt.plot(x, y / norm)
+
+    # add labeling
+    plt.xlabel(xLabel)
+    plt.ylabel(yLabel)
+
+
+def multiPlot(x, ys, xLabel, yLabel, yNames):
+    # initialize the plot
+    plt.figure()
+    plt.hold(True)
+
+    # determine automatic range scaling
+    (norm, yLabel) = getAutoRange(np.amax(np.fabs(ys)), yLabel)
+
+    # plot of the each y series
+    for y in ys:
+        plt.plot(x, y / norm)
+
+    # add labeling
+    plt.xlabel(xLabel)
+    plt.ylabel(yLabel)
+    plt.legend(yNames, loc="best")
