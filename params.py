e9904787fd1c18dbb5955922105acace87705c3b
seye42 <24215757+seye42@users.noreply.github.com>
2021-05-01 12:58:13 -0400

    Updated parameters for May 2021

--- params.py
+++ params.py
@@ -3,15 +3,14 @@ import numpy as np
 params = {}
 
 # ages
-params['startAge'] = 39.0 + 7.0 / 12
+params['startAge'] = 39.0 + 11.0 / 12
 params['stopAge']  = 100.0
 params['birthMonth'] = 5
-params['currencyYear'] = '2020'
+params['currencyYear'] = '2021'
 retirementAge = 60.0
 
 # interest rates
-APRInflation = 0.0322  # 1913-2014 US CPI average
-APRInvest    = 0.0962  # S&P 25-year annualized (1989-2014)
+APRInflation = 0.0289  # 1980-2014 US CPI average
 
 # accounts
 accounts = [\
@@ -19,23 +18,23 @@ accounts = [\
  'hasIncome'     : True,
  'minAge'        : 0.0,
  'maxAge'        : retirementAge,
- 'delMonthly'    : (4043 * 26.0 + 4500.0) / 12.0,
-   # Nov 2020 paystub with estimated annual bonus and adjustment to spread Roth 401(k) contributions over full year
+ 'delMonthly'    : (3675.0 * 26.0 + 4455.0) / 12.0,
+   # 1H2021 paystub take-home pay with estimated (based on 2020) annual bonus, includes 401(k) and HSA contributions
  'earned'        : True},
 
 {'label'         : 'QSAI retention bonus',
  'hasIncome'     : True,
  'minAge'        : 40.1,
  'maxAge'        : 40.2,
- 'delMonthly'    : 45000.0,  # $60,000 paid on ~7/1/2021 after tax withholdings
+ 'delMonthly'    : 39660.0,  # $60,000 paid on ~7/1/2021 after tax withholdings
  'earned'        : True},
 
 {'label'         : 'Social Security (Kirstin)',
  'hasIncome'     : True,
- 'minAge'        : 70.0,#62.0,
+ 'minAge'        : 67.0,#62.0,
  'maxAge'        : np.Inf,
- 'delMonthly'    : 701.0,#396.0,  # 566.0 for age 67, 701.0 for age 70
-   # from SSA estimate retrieved in Nov 2020, DOES NOT INCLUDE BENEFITS FOR SPOUSE (SO VERY CONSERVATIVE ESTIMATE)
+ 'delMonthly'    : 1660.5,#396.0,  # 566.0 for age 67, 701.0 for age 70
+   # from SSA estimate retrieved in May 2021, based on half of spouse's (at age 67) benefit
  'earned'        : False},
 
 {'label'         : 'Social Security (Sean)',
@@ -43,44 +42,47 @@ accounts = [\
  'minAge'        : 70.0,#62.0,
  'maxAge'        : np.Inf,
  'delMonthly'    : 4160.0,#2284.0,  # 3321.0 for age 67, 4160.0 for age 70
-   # from SSA estimate retrieved in Nov 2020
+   # from SSA estimate retrieved in May 2021
  'earned'        : False},
 
 {'label'         : 'household budget',
  'hasExpenses'   : True,
  'minAge'        : 0.0,
  'maxAge'        : np.Inf,
- 'delMonthly'    : -4100.0}, # $2700 Citi + $100 REI + $800 property taxes + $500 misc.
+ 'delMonthly'    : -5000.0},
+                    # based on 2020 checking account:
+                    # - $48,000 net withdrawals (with savings activity removed)
+                    # - 2 x $6,000 Roth IRA contributions
 
 {'label'         : 'taxable savings',
  'hasSavings'    : True,
  'hasBalance'    : True,
- 'initBalance'   : 60455.0,  # as of startAge
+ 'initBalance'   : 75374.0,
  'intAPR'        : 0.0434 - APRInflation},  # inflation-adjusted returns
 
 {'label'         : 'Roth IRA (Kirstin)',
  'hasBalance'    : True,
- 'initBalance'   : 136800.0,
- 'intAPR'        : 0.095162 - APRInflation,  # inflation-adjusted returns
+ 'initBalance'   : 170505.0,
+ 'intAPR'        : 0.095155 - APRInflation,  # inflation-adjusted returns
  'hasContributionLimits': True,
  'maxContrib'    : 6000.0 / 12,  # annual IRS maximum, normalized to monthly
  'phaseOutBegAGI': np.Inf,  # AGI where phase-out begins
  'phaseOutEndAGI': np.Inf},  # AGI where inelibible for contributions
-    # based on 2018 rules for married filing jointly
+    # based on 2021 rules for married filing jointly
 
 {'label'         : 'Roth IRA (Sean)',
  'hasBalance'    : True,
- 'initBalance'   : 512508.0,
- 'intAPR'        : 0.0703522 - APRInflation,  # inflation-adjusted returns
+ 'initBalance'   : 589733.0,
+ 'intAPR'        : 0.069904 - APRInflation,  # inflation-adjusted returns
  'hasContributionLimits': True,
  'maxContrib'    : 6000.0 / 12,
  'phaseOutBegAGI': np.Inf,  # AGI where phase-out begins
  'phaseOutEndAGI': np.Inf},  # AGI where inelibible for contribution
-    # based on 2018 rules for married filing jointly
+    # based on 2021 rules for married filing jointly
 
 {'label'         : 'Roth 401(k) (Sean)',
  'hasBalance'    : True,
- 'initBalance'   : 15798.0,
+ 'initBalance'   : 29070.0,
  'intAPR'        : 0.0711 - APRInflation,  # inflation-adjusted returns
  'hasContributions': True,
  'minAge'        : 0.0,
@@ -89,17 +91,17 @@ accounts = [\
 
 {'label'         : '401(k) (Sean)',
  'hasBalance'    : True,
- 'initBalance'   : 2844.0,
+ 'initBalance'   : 6465.0,
  'intAPR'        : 0.0711 - APRInflation,  # inflation-adjusted returns
  'hasContributions': True,
  'minAge'        : 0.0,
  'maxAge'        : retirementAge,
- 'delMonthly'    : 6885.0 / 12,  # 4.5% of salary
+ 'delMonthly'    : 6885.0 / 12,  # 4.5% of 1H2021 salary
  'hasRMDs'       : True},
 
 {'label'         : 'HSA',
  'hasBalance'    : True,
- 'initBalance'   : 8914.0,
+ 'initBalance'   : 11454.0,
  'intAPR'        : 0.0531 - APRInflation,  # inflation-adjusted returns
  'hasContributions': True,
  'minAge'        : 0.0,
--- paramsL.py
+++ paramsL.py
@@ -1,26 +1,26 @@
 params = {}
 
 # ages
-params['startAge'] = 11
-params['stopAge']  = 22
+params['startAge'] = 11 + 4.0 / 12.0
+params['stopAge']  = 22.0
 params['birthMonth'] = 12
-params['currencyYear'] = '2020'
+params['currencyYear'] = '2021'
 
 # interest rates
-APRInflation = 0.0322  # 1913-2014 US CPI average
-APRInvest    = 0.065025
-  # target portfolio construction based on historical per-fund returns retrieved on 11/19/16
+APRInflation = 0.0289  # 1980-2014 US CPI average
+APRInvest    = 0.07258
+  # target portfolio construction based on historical per-fund returns retrieved on 12/19/2020
 
 # accounts
 accounts = [\
 {'label'         : 'tuition & fees',
- 'hasExpense'    : True,
+ 'hasExpenses'   : True,
  'minAge'        : 18.75,
  'maxAge'        : 22.75,
  'delMonthly'    : -1265.0},  # average across universities retrieved on 1/1/19
 
 {'label'         : 'household budget',
- 'hasExpense'    : True,
+ 'hasExpenses'    : True,
  'minAge'        : 18.75,
  'maxAge'        : 22.75,
  'delMonthly'    : -750.0},
@@ -28,5 +28,5 @@ accounts = [\
 {'label'         : 'UTMA',
  'hasSavings'    : True,
  'hasBalance'    : True,
- 'initBalance'   : 104290.0,  # as of startAge
+ 'initBalance'   : 119987.0,  # as of startAge
  'intAPR'        : APRInvest - APRInflation}]  # inflation-adjusted returns
