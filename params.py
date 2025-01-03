1384b9a104915bb7f212a8bb3e621d12134b8891
seye42 <24215757+seye42@users.noreply.github.com>
2025-01-03 14:49:21 -0500

    Updated balances, monthly values, and rates

--- params.py
+++ params.py
@@ -4,15 +4,15 @@ import finUtils
 params = {}
 
 # ages
-params['startAge'] = 43.0 + 1.0 / 12
+params['startAge'] = 43.0 + 8.0 / 12
 params['stopAge']  = 100.0
 params['birthMonth'] = 5
-params['currencyYear'] = '2024'
-retirementAge = 60.0
+params['currencyYear'] = '2025'
+retirementAge = 55.0
 
 # interest rates
-APRInflation = 0.0308675  # 1980 to 2024 US CPI annualized average
-  # from https://www.usinflationcalculator.com/, with i = 3.81 ** (1/44) - 1
+APRInflation = 0.0309902  # 1980 to 2025 US CPI annualized average
+  # from https://www.usinflationcalculator.com/, with i = 3.83 ** (1/44) - 1
 
 # accounts
 accounts = [\
@@ -20,43 +20,43 @@ accounts = [\
  'hasIncome'     : True,
  'minAge'        : 0.0,
  'maxAge'        : retirementAge,
- 'delMonthly'    : (4456.0 * 26.0 + 35000.0) / 12.0,
-    # 2Q2024 paystub take-home pay with estimated annual bonus, includes 401(k) and HSA contributions
+ 'delMonthly'    : (4861.0 * 26.0 + 29745.0) / 12.0,
+    # 4Q2024 paystub take-home pay with estimated annual bonus, includes 401(k) and HSA contributions
  'earned'        : True},
 
 {'label'         : 'Social Security (K)',
  'hasIncome'     : True,
  'minAge'        : 62.0,
  'maxAge'        : np.Inf,
- 'delMonthly'    : 1320.5,
-    # from S SSA estimate retrieved in Jun 2024, based on half of spouse's (at age 62) benefit
+ 'delMonthly'    : 0.5 * 2722.0,
+    # from S SSA estimate retrieved in Jan 2025, based on half of spouse's (at age 62) benefit
  'earned'        : False},
 
 {'label'         : 'Social Security (S)',
  'hasIncome'     : True,
  'minAge'        : 62.0,
  'maxAge'        : np.Inf,
- 'delMonthly'    : 2641.0,  # 2641.0 for age 62, 3827.0 for age 67, 4785.0 for age 70
-    # from SSA estimate retrieved in Jun 2024
+ 'delMonthly'    : 2722.0,  # 2722.0 for age 62, 3933.0 for age 67, 4908.0 for age 70
+    # from SSA estimate retrieved in Jan 2025
  'earned'        : False},
 
 {'label'         : 'household budget',
  'hasExpenses'   : True,
  'minAge'        : 0.0,
  'maxAge'        : np.Inf,
- 'delMonthly'    : -6265.0},
-    # based on 2023 annual total expenses +5%, less the ones that are captured elsewhere here
+ 'delMonthly'    : -6553.0},
+    # based on 2024 annual total expenses +5%, less the ones that are captured elsewhere here
 
 {'label'         : 'taxable savings',
  'hasSavings'    : True,
  'hasBalance'    : True,
- 'initBalance'   : 270070.7,
- 'intAPR'        : finUtils.adjustForInflation(0.0431, APRInflation)},  # inflation-adjusted returns
+ 'initBalance'   : 304666.18,
+ 'intAPR'        : finUtils.adjustForInflation(0.0441, APRInflation)},  # inflation-adjusted returns
 
 {'label'         : 'Roth IRA (K)',
  'hasBalance'    : True,
- 'initBalance'   : 187079.39,
- 'intAPR'        : finUtils.adjustForInflation(0.0892, APRInflation),  # inflation-adjusted returns
+ 'initBalance'   : 206150.32,
+ 'intAPR'        : finUtils.adjustForInflation(0.0904, APRInflation),  # inflation-adjusted returns
  'hasContributionLimits': True,
  'maxContrib'    : 7000.0 / 12,  # annual IRS maximum, normalized to monthly
  'phaseOutBegAGI': np.Inf,  # AGI where phase-out begins
@@ -65,8 +65,8 @@ accounts = [\
 
 {'label'         : 'Roth IRA (S)',
  'hasBalance'    : True,
- 'initBalance'   : 681385.89,
- 'intAPR'        : finUtils.adjustForInflation(0.0724, APRInflation),  # inflation-adjusted returns
+ 'initBalance'   : 735938.59,
+ 'intAPR'        : finUtils.adjustForInflation(0.0793, APRInflation),  # inflation-adjusted returns
  'hasContributionLimits': True,
  'maxContrib'    : 7000.0 / 12,
  'phaseOutBegAGI': np.Inf,  # AGI where phase-out begins
@@ -75,36 +75,36 @@ accounts = [\
 
 {'label'         : 'Roth 401(k) (S)',
  'hasBalance'    : True,
- 'initBalance'   : 121105.03,
- 'intAPR'        : finUtils.adjustForInflation(0.0743, APRInflation),  # inflation-adjusted returns
+ 'initBalance'   : 144419.78,
+ 'intAPR'        : finUtils.adjustForInflation(0.0829, APRInflation),  # inflation-adjusted returns
  'hasContributions': True,
  'minAge'        : 0.0,
  'maxAge'        : retirementAge,
- 'delMonthly'    : 23000.0 / 12},  # enough to guarantee match in 401(k) portion
+ 'delMonthly'    : 23500.0 / 12},  # enough to guarantee match in 401(k) portion
 
 {'label'         : '401(k) (S)',
  'hasBalance'    : True,
- 'initBalance'   : 39665.75,
- 'intAPR'        : finUtils.adjustForInflation(0.0743, APRInflation),  # inflation-adjusted returns
+ 'initBalance'   : 47762.99,
+ 'intAPR'        : finUtils.adjustForInflation(0.0829, APRInflation),  # inflation-adjusted returns
  'hasContributions': True,
  'minAge'        : 0.0,
  'maxAge'        : retirementAge,
- 'delMonthly'    : 8326.12 / 12,  # 4.5% of 2Q2024 salary
+ 'delMonthly'    : 8592.55 / 12,  # 4.5% of 4Q2024 salary
  'hasRMDs'       : True},
 
 {'label'         : 'HSA',
  'hasBalance'    : True,
- 'initBalance'   : 33564.39,
- 'intAPR'        : finUtils.adjustForInflation(0.0450, APRInflation),  # inflation-adjusted returns
+ 'initBalance'   : 38495.51,
+ 'intAPR'        : finUtils.adjustForInflation(0.0437, APRInflation),  # inflation-adjusted returns
  'hasContributions': True,
  'minAge'        : 0.0,
  'maxAge'        : retirementAge,
- 'delMonthly'    : 8300.0 / 12},
+ 'delMonthly'    : 8550.0 / 12},
 
 {'label'         : 'house',
  'hasBalance'    : True,
- 'initBalance'   : 526200.0,  # Zillow estimate from 6/22/2024 for recent Devonshire floorplans
- 'intAPR'        : 0.00989,  # inflation-adjusted returns
+ 'initBalance'   : 505900.0,  # Zillow estimate from Jan 2025 for recent Devonshire floorplans
+ 'intAPR'        : 0.00825,  # inflation-adjusted returns
     # based on Farrand's purchase price of $206,145 on 4/16/1997, the Zillow estimate above, and inflation over the
     # intervening period from https://www.usinflationcalculator.com/
  'hasContributions': False,
