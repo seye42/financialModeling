27188b632b65cb76a85388aa0984a40db4dda421
seye42 <24215757+seye42@users.noreply.github.com>
2026-05-23 14:50:02 -0400

    Refactored to use yfinance for automatically retrieving historical share data

--- portfolioAnalysis.py
+++ portfolioAnalysis.py
@@ -1,3 +1,4 @@
+import yfinance as yf
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
@@ -6,55 +7,33 @@ import pandas as pd
 
 import finUtils
 
-# TODO: Should all of this be in a class so that all related data are more easily accessible?
-# TODO: Document where the CSV data come from.
 
-def parseTickerCSV(filename):
-    # extract the ticker symbol
-    path, name = os.path.split(filename)
-    ticker = name.replace('.csv', '')
+def getPortfolioData(portfolio):
+    # check the weights
+    if sum(portfolio.values()) != 1.0:
+        raise ValueError('bad weights(s)')
 
-    # parse the date and closing value time series data
-    df = pd.read_csv(filename)
-    dates = pd.to_datetime(df['Date'].to_numpy())
-    values = df['Close'].to_numpy()
+    # retrieve the data
+    data = yf.Tickers(' '.join(portfolio.keys())).history(period='max', progress=False)
 
-    return (ticker, dates, values)
+    # check that all tickers were retrievable
+    if any(data.isnull().all()):  # at least one column is entirely null
+        # yfinance emits download error console messages for tickers it can't find but doesn't raise an exception. The
+        # columns corresponding to a missing ticker have NaN values for every row.
+        raise ValueError('bad ticker symbol(s)')
 
-
-def parseAllTickerCSVs(filenames):
-    # process each file
-    tickers = []
-    dates = []
-    initialDates = []
-    finalDates = []
-    values = []
-    for file in filenames:
-        currTicker, currDates, currValues = parseTickerCSV(file)
-        tickers.append(currTicker)
-        dates.append(currDates)
-        initialDates.append(currDates[0])
-        finalDates.append(currDates[-1])
-        values.append(currValues)
-        print(currTicker + ' ' + str(currDates[0]))
-
-    # determine the limiting date range across all tickers
-    startDate = max(initialDates)
-    stopDate = min(finalDates)
-    print('keeping ' + str(startDate) + ' to ' + str(stopDate))
-
-    # trim the time span of each series and build up the ticker dictionary
-    d = {}
-    for i in range(len(tickers)):
-        startIdx = np.searchsorted(dates[i], startDate)
-        stopIdx = np.searchsorted(dates[i], stopDate)
-        if i == 0:
-            t = dates[i][startIdx:stopIdx]
-        d[tickers[i]] = values[i][startIdx:stopIdx]
-
-    return (t, d)
+    # trim to the common time range and closing value columns
+    return data['Close'].dropna()
 
 
+def normalizePortfolioData(data):
+    for column in data.columns:
+        data[column] /= data[column].iloc[0]
+
+    return data
+
+
+# TODO: needed?
 def combineTickers(d, weights):
     if sum(weights.values()) != 1.0:
         raise ValueError('bad weights')
@@ -65,6 +44,7 @@ def combineTickers(d, weights):
     return c
 
 
+# TODO: needed?
 def generateFixedRateSeries(t, rAnnual):
     rDaily = finUtils.convAPRtoDay(rAnnual)
     s = np.zeros(t.shape)
@@ -76,6 +56,7 @@ def generateFixedRateSeries(t, rAnnual):
     return s
 
 
+# TODO: needed?
 def includeFees(t, s, rAnnual):
     rDaily = finUtils.convAPRtoDay(rAnnual)
     corr = 1.0
@@ -89,39 +70,6 @@ def includeFees(t, s, rAnnual):
     return c
 
 
-files = ['/home/seye/Repositories/financialModeling/data/VEMAX.csv',
-         '/home/seye/Repositories/financialModeling/data/VFIAX.csv',
-         '/home/seye/Repositories/financialModeling/data/VGSLX.csv',
-         '/home/seye/Repositories/financialModeling/data/VIPSX.csv',
-         '/home/seye/Repositories/financialModeling/data/VSMAX.csv',
-         '/home/seye/Repositories/financialModeling/data/VTMGX.csv',
-         '/home/seye/Repositories/financialModeling/data/VUSTX.csv',
-         # '/home/seye/Repositories/financialModeling/data/VTIAX.csv',
-         '/home/seye/Repositories/financialModeling/data/VBTLX.csv',
-         '/home/seye/Repositories/financialModeling/data/VTSAX.csv']
-         # '/home/seye/Repositories/financialModeling/data/VSBSX.csv',
-         # '/home/seye/Repositories/financialModeling/data/VGLT.csv',
-         # '/home/seye/Repositories/financialModeling/data/VMBS.csv',
-         # '/home/seye/Repositories/financialModeling/data/VGIT.csv',
-         # '/home/seye/Repositories/financialModeling/data/VGSH.csv',
-         # '/home/seye/Repositories/financialModeling/data/SCHP.csv',
-         # '/home/seye/Repositories/financialModeling/data/USIG.csv',
-         # '/home/seye/Repositories/financialModeling/data/SPSM.csv',
-         # '/home/seye/Repositories/financialModeling/data/IEMG.csv',
-         # '/home/seye/Repositories/financialModeling/data/IWY.csv',
-         # '/home/seye/Repositories/financialModeling/data/IWX.csv',
-         # '/home/seye/Repositories/financialModeling/data/EFG.csv',
-         # '/home/seye/Repositories/financialModeling/data/EFV.csv',
-         # '/home/seye/Repositories/financialModeling/data/IWP.csv',
-         # '/home/seye/Repositories/financialModeling/data/IWS.csv',
-         # '/home/seye/Repositories/financialModeling/data/PRWCX.csv',
-         # '/home/seye/Repositories/financialModeling/data/VINIX.csv',
-         # '/home/seye/Repositories/financialModeling/data/FCNTX.csv',
-         # '/home/seye/Repositories/financialModeling/data/VMCIX.csv',
-         # '/home/seye/Repositories/financialModeling/data/VSCIX.csv',
-         # '/home/seye/Repositories/financialModeling/data/VWINX.csv',
-         # '/home/seye/Repositories/financialModeling/data/VGHCX.csv']
-
 weightsEye = {'VEMAX': 0.05,
               'VFIAX': 0.30,
               'VGSLX': 0.10,
@@ -131,6 +79,8 @@ weightsEye = {'VEMAX': 0.05,
               'VUSTX': 0.05}
 weightsBogleYoung = {'VTSAX': 0.80,
                      'VBTLX': 0.20}
+weightsExpenseTest = {'VUSTX': 0.5,
+                      'VUSUX': 0.5}
 # weightsBogleMiddle = {'VTSAX': 0.45,
 #                       'VTIAX': 0.10,
 #                       'VGSLX': 0.05,
@@ -173,51 +123,3 @@ weightsBogleYoung = {'VTSAX': 0.80,
   # Int'l Stocks: Large, Large Value, and Small -> VTMGX (VFSAX exists for small-cap, but has erratic/incomplete historical data)
   # Int'l Stocks: Emerging Markets -> VEMAX
   # U.S. Two-year -> VSBSX
-
-# parse the historical time series data
-t, dUnnorm = parseAllTickerCSVs(files)
-
-# normalize to starting dollars (not price per share)
-d = {}
-for key in dUnnorm:
-    d[key] = dUnnorm[key] / dUnnorm[key][0]
-
-# simulate custom portfolios
-c= {}
-c['Eye'] = combineTickers(d, weightsEye)
-c['Bogleheads Young'] = combineTickers(d, weightsBogleYoung)
-# c['Bogleheads Middle'] = combineTickers(d, weightsBogleMiddle)
-# c['Bogleheads Early Retirement'] = combineTickers(d, weightsBogleEarlyRet)
-# c['Bogleheads Late Retirement'] = combineTickers(d, weightsBogleLateRet)
-# c['Swedroe Conservative'] = combineTickers(d, weightsSwedroeConvserv)
-# c['Swedroe Moderate'] = combineTickers(d, weightsSwedroeModerate)
-# c['Swedroe Moderately Aggressive'] = combineTickers(d, weightsSwedroeModAggr)
-# c['Swedroe Highly Aggressive'] = combineTickers(d, weightsSwedroeHighAggr)
-
-# plot closing values
-matplotlib.rcParams.update({'font.size': 20, 'figure.facecolor': 'w', 'lines.linewidth': 2})
-plt.figure()
-for key in dUnnorm:
-    plt.plot(t, dUnnorm[key], label=key)
-plt.xlabel('time')
-plt.ylabel('closing value ($)')
-plt.legend()
-plt.grid()
-
-# plot normalized values of core funds
-plt.figure()
-for key in d.keys():
-    plt.plot(t, d[key], label=key)
-plt.xlabel('time')
-plt.ylabel('normalized value')
-plt.legend()
-plt.grid()
-
-# plot normalized values of weighted portfolios
-plt.figure()
-for key in c.keys():
-    plt.plot(t, c[key], label=key)
-plt.xlabel('time')
-plt.ylabel('normalized value')
-plt.legend()
-plt.grid()
